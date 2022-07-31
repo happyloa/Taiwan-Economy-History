185213089a92bbd946718bc701cf76cb606990f7
Wolfpack <piafyoyo06@gmail.com>
2022-07-31 12:04:01 +0800

    feat: 新增註解

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+// 引入全域樣式
 import './global.css'
+// 引入 Taiwan Icon
 import './assets/font/twicon.css'
+// 引入 App.svelte
 import App from './App.svelte'
 
+// 將 App 綁在 index.html 的 #app 上
 const app = new App({
   target: document.getElementById('app')
 })
