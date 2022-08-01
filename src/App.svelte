990b865d194fca8a66203de283377755ba264c11
Wolfpack <piafyoyo06@gmail.com>
2022-08-01 20:40:59 +0800

    feat: 將 before heading 拆分成元件

--- src/App.svelte
+++ src/App.svelte
@@ -12,20 +12,16 @@
   // 引入 Author 元件
   import Author from "./lib/Author/Author.svelte";
 
+  // 引入 beforeHeader 元件
+  import BeforeHeader from "./lib/before_Header/before_Header.svelte";
   // 引入 Footer 元件
   import Footer from "./lib/Footer/Footer.svelte";
 
   export let url = "";
 </script>
 
-<header class="container-fluid bg-light p-3">
-  <div class="row">
-    <div class="col-12">
-      <h1 class="text-center text-lg-start"><i class="twicon-main-island fs-1" /> 台灣經濟發展歷史</h1>
-      <p class="text-center text-lg-start mb-0 mb-md-2">過去70年台灣經濟發展的歷史以及相關政策</p>
-    </div>
-  </div>
-</header>
+<!-- 這裡是 BeforeHeader -->
+<BeforeHeader />
 
 <Router url="{url}">
   <!--這裡是nav bar-->
---
+++ src/lib/before_Header/before_header.svelte
@@ -0,0 +1,12 @@
+<header class="container-fluid bg-light p-3">
+  <div class="row">
+    <div class="col-12">
+      <h1 class="text-center text-lg-start">
+        <i class="twicon-main-island fs-1" /> 台灣經濟發展歷史
+      </h1>
+      <p class="text-center text-lg-start mb-0 mb-md-2">
+        過去70年台灣經濟發展的歷史以及相關政策
+      </p>
+    </div>
+  </div>
+</header>
