1b40633e29ed70b2b17b6d1b8ffc86e19286ea71
Wolfpack <piafyoyo06@gmail.com>
2022-08-01 19:18:53 +0800

    feat: 加入 Bootstrap 樣式

--- src/lib/Footer/Footer.svelte
+++ src/lib/Footer/Footer.svelte
@@ -4,29 +4,14 @@
   let title = "前往 GitHub 儲存庫";
 </script>
 
-<footer>
-  <p>台灣經濟發展歷史 by 謝宗佑</p>
+<footer class="container-fluid py-3 bg-dark text-center">
+  <p class="mb-2 text-light fs-5">台灣經濟發展歷史 by 謝宗佑</p>
   <a href="https://github.com/happyloa/Taiwan-Economy-History" target="_blank"
-    ><img {src} {alt} {title} /></a
+    ><img class="img-fluid" {src} {alt} {title} /></a
   >
 </footer>
 
 <style>
-  footer {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    padding: 1rem 0;
-    background-color: #312f2f;
-  }
-
-  footer p {
-    font-size: 18px;
-    color: white;
-    margin-bottom: 10px;
-  }
-
   footer img {
     width: 50px;
   }
