8bd840f71f6898af00573b22f9d73da443f66ce7
Wolfpack <piafyoyo06@gmail.com>
2022-08-09 19:37:13 +0800

    test form

--- src/lib/Home/Home.svelte
+++ src/lib/Home/Home.svelte
@@ -72,6 +72,17 @@
         aria-hidden="false"
         title="台灣地理位置"
       />
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </section>
   </div>
 </section>
